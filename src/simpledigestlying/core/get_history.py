99245b4fdee424121394d42505683ace45a46aa0
Silletr <silletrMain@gmail.com>
2026-05-28 12:51:49 +0300

    [CHANGED FILE/DIR: src/simpledigestlying/core/get_history.py, src/simpledigestlying/core/handlers.py | REMOVED FILE/DIR: src/simpledigesetlying/ai_digest/get_history] [Type: Added command for history printing] ..and a bit of cleaning up - deleted get_history.py from ai_digest folder because it's not belong to it anymore (yes). And yeah, I took some vacation, and I guess I'll do everything I can today and go again for 1-2 days xD [TIMESTAMP: 28/05/2026 | 12:51 UTC+3]

--- src/simpledigestlying/core/get_history.py
+++ src/simpledigestlying/core/get_history.py
@@ -3,5 +3,3 @@ Well.. It's gonna take history of DM, as for start,
 including all messages (10, 20, 30, 500 - doesn't matter)
 And then save it to the file and send it to the AI
 """
-
-from dispatcher import dispatcher
--- src/simpledigestlying/core/handlers.py
+++ src/simpledigestlying/core/handlers.py
@@ -1,8 +1,21 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+
+#    ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+#    ┃    get messages from the last 24h    ┃
+#    ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+today = datetime.now(tz=ZoneInfo("Europe/Kyiv"))
 
 
 def register_handlers(app: Client):
     @app.on_message(filters.command("start") & filters.private)
     async def start_handler(client: Client, message: Message):
         await message.reply("Hi. I am alive.")
+
+    @app.on_message(filters.command("history") & filters.private)
+    async def history_handler(client: Client, message: Message):
+        today = datetime.now(tz=ZoneInfo("Europe/Kyiv"))
+        async for msg in app.get_chat_history(chat_id="me", offset_date=today):
+            print(msg.text)
