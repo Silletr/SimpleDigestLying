042f6fb27faf28d3bb2e7b516121bea8d3878bfa
Silletr <silletrMain@gmail.com>
2026-05-29 21:15:12 +0300

    [CHANGED FILE/DIR: src/simpledigestlying/core/get_history.py, src/simpledigestlying/core/handlers.py] [Type: Made logging system] And btw I made the /history comand work  as well. And well.. at least I'm not that drunk as I could be, soo.. I'm available for 2 commits today xD. Idk why I took so much break, but yes [TIMESTAMP: 29/05/2026 | 21:15 UTC+3]

--- src/simpledigestlying/core/get_history.py
+++
@@ -1,5 +0,0 @@
-"""
-Well.. It's gonna take history of DM, as for start,
-including all messages (10, 20, 30, 500 - doesn't matter)
-And then save it to the file and send it to the AI
-"""
--- src/simpledigestlying/core/handlers.py
+++ src/simpledigestlying/core/handlers.py
@@ -1,12 +1,20 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from datetime import datetime, timedelta
+from datetime import datetime
 from zoneinfo import ZoneInfo
+from loguru import logger
+import tracemalloc
 
+tracemalloc.start()
 #    ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
 #    ┃    get messages from the last 24h    ┃
 #    ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 today = datetime.now(tz=ZoneInfo("Europe/Kyiv"))
+logger.add(
+    sink="../logs/handlers.py.log",
+    level="INFO",
+    format="{time:DD/MM/YYYY HH:mm:ss} | {level} | {message}",
+)
 
 
 def register_handlers(app: Client):
@@ -16,6 +24,16 @@ def register_handlers(app: Client):
 
     @app.on_message(filters.command("history") & filters.private)
     async def history_handler(client: Client, message: Message):
-        today = datetime.now(tz=ZoneInfo("Europe/Kyiv"))
-        async for msg in app.get_chat_history(chat_id="me", offset_date=today):
-            print(msg.text)
+        async for msg in app.get_chat_history(
+            chat_id=message.chat.id, offset_date=today
+        ):
+            logger.success(
+                f"Message text: {msg.text} | Message sender: {
+                    msg.from_user.username if msg.from_user else 'unknown'
+                }"
+            )
+
+        await client.send_message(
+            chat_id=message.chat.id,
+            text="Successfully sent history to the ../logs/handlers.py.log. Check it out right fcking now",
+        )
